2971e9eba06e0f4150f2ddf8e9b7ec2e0ad559c8
Niltok <72703236+niltok64@users.noreply.github.com>
2022-03-04 11:57:59 +0000

    Delete lovebrew3ds directory

--- lovebrew3ds/LVBREW3DS/main.lua
+++
@@ -1,8 +0,0 @@
-function love.draw()
-    love.graphics.print('Hello World!', 200, 120)
-end
-
--- we need to quit the app when a button is pressed
-function love.gamepadpressed(joystick, button)
-    love.event.quit()
-end
