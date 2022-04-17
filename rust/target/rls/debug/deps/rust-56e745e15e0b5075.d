43ae9d3eadf937dc4008d3d2da2f172bf1a7a1b2
niltok64 <louissaitomaloberti@gmail.com>
2022-04-17 11:29:43 +0100

    replace rust with cargo shit

---
+++ rust/target/rls/debug/deps/rust-56e745e15e0b5075.d
@@ -0,0 +1,5 @@
+/home/louis/Development/hello-worlds/rust/target/rls/debug/deps/rust-56e745e15e0b5075.rmeta: src/main.rs
+
+/home/louis/Development/hello-worlds/rust/target/rls/debug/deps/rust-56e745e15e0b5075.d: src/main.rs
+
+src/main.rs:
---
+++ rust/target/rls/debug/deps/rust-791841506f362c10.d
@@ -0,0 +1,5 @@
+/home/louis/Development/hello-worlds/rust/target/rls/debug/deps/rust-791841506f362c10.rmeta: src/main.rs
+
+/home/louis/Development/hello-worlds/rust/target/rls/debug/deps/rust-791841506f362c10.d: src/main.rs
+
+src/main.rs:
